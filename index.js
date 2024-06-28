aa4e6dbeb569442a1afe6050556c7f6ae120bda4
Karman0068 <97960844+Karman0068@users.noreply.github.com>
2024-06-28 20:53:16 +0530

    jwt - cookie implemented

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const staticRoute = require("./routes/staticRouter");
 const userRoute = require("./routes/user");
 
 const app = express();
-const PORT = 8002;
+const PORT = 8003;
 
 connectToMongoDB(process.env.MONGODB ?? "mongodb://localhost:27017/short-url").then(() =>
   console.log("Mongodb connected")
